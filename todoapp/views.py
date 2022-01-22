b1811a656214d2d7daa1a6a2afd9c94069d390b3
Evgeny <9141893@mail.ru>
2022-01-22 17:04:21 +0300

    Lesson 4

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,13 +1,36 @@
 from rest_framework.viewsets import ModelViewSet
 from .serializers import ProjectSerializer, ToDoSerializer
 from .models import Project, ToDo
+from rest_framework.response import Response
+from rest_framework.pagination import LimitOffsetPagination
+
+
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
 
 
 class ProjectViewSet(ModelViewSet):
     serializer_class = ProjectSerializer
     queryset = Project.objects.all()
+    pagination_class = ProjectLimitOffsetPagination
+
+    # def get_queryset(self):
+    #     return Project.objects.filter(name__contains='new')
+
+
+class ToDoLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 20
 
 
 class ToDoViewSet(ModelViewSet):
     serializer_class = ToDoSerializer
     queryset = ToDo.objects.all()
+    filterset_fields = ['project', 'text',
+                        'create', 'update', 'creator', 'is_active']
+    pagination_class = ToDoLimitOffsetPagination
+
+    def destroy(self, request, *args, **kwargs):
+        todo = self.get_object().ToDo
+        todo.is_active = False
+        todo.save()
+        return Response(ToDoSerializer(self.get_object()).data)
--- user/views.py
+++ user/views.py
@@ -1,8 +1,9 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from .models import User
 from .serializers import UserModelSerializer
+from rest_framework import mixins
 
 
-class UserModelViewSet(ModelViewSet):
+class UserModelViewSet(mixins.UpdateModelMixin, mixins.RetrieveModelMixin, mixins.ListModelMixin, GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
