14b3c290b3697726f8c7e7fb971438ec87455a7a
MonakhovEvgeny <79536395+MonakhovEvgeny@users.noreply.github.com>
2022-02-12 12:17:00 +0300

    Lesson 6 (#6)

--- user/views.py
+++ user/views.py
@@ -2,8 +2,10 @@ from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from .models import User
 from .serializers import UserModelSerializer
 from rest_framework import mixins
+from rest_framework.permissions import AllowAny
 
 
-class UserModelViewSet(mixins.UpdateModelMixin, mixins.RetrieveModelMixin, mixins.ListModelMixin, GenericViewSet):
+class UserModelViewSet(ModelViewSet):
+    # permission_classes = [AllowAny]
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
